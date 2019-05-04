e8b3eef9585d56eeff5ffc600652eabdc612959f
VincentJ <im.vincentj@gmail.com>
2019-05-04 23:53:11 +0800

    Initial commit

---
+++ src/t1/MyStack.java
@@ -0,0 +1,49 @@
+package t1;
+
+import java.util.Stack;
+
+/**
+ * 思路：双栈，一个正常保存数据，一个用来存储最小值（当前数据栈中的最小值，有重复的）。
+ *
+ * @author VincentJ
+ * @date 2019-05-04
+ */
+public class MyStack {
+    private Stack<Integer> stackData;
+    private Stack<Integer> stackMin;
+
+    public MyStack(Stack<Integer> stackData, Stack<Integer> stackMin) {
+        this.stackData = stackData;
+        this.stackMin = stackMin;
+    }
+
+
+    public int getMin() {
+        if (this.stackMin.isEmpty()) {
+            throw new RuntimeException("MinStack is empty.");
+        }
+        return this.stackMin.peek();
+    }
+
+    public void push(int pNewNum) {
+        this.stackData.push(pNewNum);
+
+        if (this.stackMin.isEmpty()) {
+            this.stackMin.push(pNewNum);
+        } else if (this.getMin() > pNewNum) {
+            this.stackMin.push(pNewNum);
+        } else {
+            int curMin = this.getMin();
+            this.stackMin.push(curMin);
+        }
+    }
+
+    public int pop() {
+        if (this.stackData.isEmpty()) {
+            throw new RuntimeException("DataStack is empty.");
+        }
+        this.stackMin.pop();
+        return this.stackData.pop();
+    }
+
+}
---
+++ src/t2/TwoStackQueue.java
@@ -0,0 +1,59 @@
+package t2;
+
+import java.util.Stack;
+
+/**
+ * 思路：栈是FILO，而队列是FIFO，双栈刚好把顺序反过来（一个作为压入栈，一个作为弹出栈）.
+ * 注意点：在往弹出栈导数据时，要保证一次性；弹出栈只要不为空就不能往里面导数据。
+ *
+ * @author VincentJ
+ * @date 2019-05-04
+ */
+public class TwoStackQueue {
+    /**
+     * 压入栈
+     */
+    private Stack<Integer> stackPush;
+    /**
+     * 弹出栈
+     */
+    private Stack<Integer> stackPop;
+
+    public TwoStackQueue(Stack<Integer> stackPush, Stack<Integer> stackPop) {
+        this.stackPush = stackPush;
+        this.stackPop = stackPop;
+    }
+
+    /**
+     * 压入数据的时机可以有多个，这里选择在add的时候
+     */
+    private void pushToPop() {
+        // 只有为空了才能导数据
+        if (this.stackPop.isEmpty()) {
+            while (!stackPush.isEmpty()) {
+                stackPop.push(stackPush.pop());
+            }
+        }
+    }
+
+    public void add(int pNewNum) {
+        this.stackPush.push(pNewNum);
+        pushToPop();
+    }
+
+    public int poll() {
+        if (stackPush.isEmpty() && stackPop.isEmpty()) {
+            throw new RuntimeException("Queue is empty.");
+        }
+        // pushToPop();
+        return stackPush.pop();
+    }
+
+    public int peek() {
+        if (stackPush.isEmpty() && stackPop.isEmpty()) {
+            throw new RuntimeException("Queue is empty.");
+        }
+        // pushToPop();
+        return stackPop.pop();
+    }
+}
